98560807afb20cac6a72a9e9d5266bf0b1575e59
paperwing <paperwing@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-01-17 23:37:21 +0000

    Updated shape drawing class, may consider splitting shapes to different classes to make it easier to replace implementations of shapes as well as adding/removing shapes

--- src/main/java/org/cytoscape/paperwing/internal/rendering/shapes/ScalableShapeDrawer.java
+++ src/main/java/org/cytoscape/paperwing/internal/rendering/shapes/ScalableShapeDrawer.java
@@ -8,9 +8,17 @@ import javax.media.opengl.glu.GLU;
 import javax.media.opengl.glu.GLUquadric;
 
 public class ScalableShapeDrawer {
+	
+	private static final int SPHERE_SLICES_DETAIL = 12;
+	private static final int SPHERE_STACKS_DETAIL = 12;
+	
 	public static enum ShapeType {
+		SHAPE_SPHERICAL,
 		SHAPE_CUBIC, 
-		SHAPE_TETRAHEDRAL
+		SHAPE_TETRAHEDRAL,
+		SHAPE_CONICAL,
+		SHAPE_CYLINDRICAL,
+		SHAPE_DOUGHNUT,
 	}
 	
 	private Map<ShapeType, Integer> shapeLists;
@@ -20,6 +28,12 @@ public class ScalableShapeDrawer {
 	}
 	
 	public void initialize(GL2 gl) {
+		initializeSphere(gl);
+		initializeCube(gl);
+	}
+	
+	// Diameter 1 sphere
+	private void initializeSphere(GL2 gl) {
 		int shapeListIndex = gl.glGenLists(1);
 
 		GLU glu = GLU.createGLU(gl);
@@ -28,16 +42,27 @@ public class ScalableShapeDrawer {
 		glu.gluQuadricDrawStyle(quadric, GLU.GLU_FILL);
 		glu.gluQuadricNormals(quadric, GLU.GLU_SMOOTH);
 
-		// Draw Shape
-		// ---------
-
 		gl.glNewList(shapeListIndex, GL2.GL_COMPILE);
-		
-		
+		glu.gluSphere(quadric, 0.5, SPHERE_SLICES_DETAIL, SPHERE_STACKS_DETAIL); 
 		gl.glEndList();
+		
+		shapeLists.put(ShapeType.SHAPE_SPHERICAL, shapeListIndex);
 	}
 	
-	
+	// Cube inscribed in a radius 0.5 sphere
+	private void initializeCube(GL2 gl) {
+		int shapeListIndex = gl.glGenLists(1);
+
+		double cornerCoordinate = 0.5 / Math.sqrt(2);
+		
+		gl.glNewList(shapeListIndex, GL2.GL_COMPILE);
+		gl.glBegin(GL2.GL_TRIANGLE_STRIP);
+		
+		gl.glEnd();
+		gl.glEndList();
+		
+		shapeLists.put(ShapeType.SHAPE_CUBIC, shapeListIndex);
+	}
 	
 	public void drawShape(ShapeType shapeType) {
 		
