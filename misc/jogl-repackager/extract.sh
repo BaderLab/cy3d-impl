9b160e7743926f92273b29d545a9a0b81eb12478
paperwing <paperwing@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-02-24 22:28:50 +0000

    Updated scripts for rebuilding JOGL as OSGi bundles; added header data for Bundle-NativeCode to include native libraries

--- misc/jogl-repackager/extract.sh
+++ misc/jogl-repackager/extract.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mkdir -p extracted_jars
-mkdir -p extracted_native_jars
 
 mkdir -p combined_jars
 mkdir -p bundles
+
+mkdir -p extracted_natives
 
 # open archives
 for i in downloads/*.7z
@@ -15,6 +16,9 @@ done
 
 # create list of files for comparison
 # pushd extract; for i in *; do pushd $i; find . >files; popd; done; popd
+
+# process jogl jar
+# ================
 
 # move non native library jars
 for i in `find extract | grep '\.jar$' | grep -v natives`
@@ -30,6 +34,7 @@ do
 done
 
 # combine into single jar
+echo Combining to single jar..
 pushd extracted_jars/classes/
 jar cf jogl.jar *
 popd
@@ -42,23 +47,107 @@ java -jar bnd.jar wrap combined_jars/jogl.jar
 mv combined_jars/jogl.bar bundles/jogl.jar
 
 
+# native library jar
+# ==================
+
 # move native jars
 for i in `find extract | grep '\.jar$' | grep natives`
 do
-	mv $i extracted_native_jars/`basename $i`
+	mv $i extracted_natives/`basename $i`
+done
+
+# move native library files
+for i in extract/*
+do
+	dest_dir=extracted_natives/`echo $i | cut -d'-' -f5,6`
+	mkdir -p $dest_dir
+
+	for j in `find $i/lib -type f`
+	do
+		cp $j $dest_dir
+	done
 done
 
 # add to single jar
-pushd extracted_native_jars
+pushd extracted_natives
 jar cf jogl-natives.jar *
 popd
-mv extracted_native_jars/jogl-natives.jar combined_jars/jogl-natives.jar
+mv extracted_natives/jogl-natives.jar combined_jars/jogl-natives.jar
 
 # generate properties file for combined natives jar
-echo "Fragment-Host=jogl" >combined_jars/jogl-natives.jar.properties
+properties_file=combined_jars/jogl-natives.jar.properties
+rm $properties_file
+
+echo -n "Bundle-NativeCode: " >>$properties_file
+
+# linux x32 properties
+for i in `find extracted_natives/linux-i586 -type f`
+do
+	echo linux-i586/`basename $i`\; >>$properties_file
+done
+
+echo osname=linux\; >>$properties_file
+echo processor=x86, >>$properties_file
+
+# linux x64 properties
+for i in `find extracted_natives/linux-amd64 -type f`
+do
+	echo linux-amd64/`basename $i`\; >>$properties_file
+done
+
+echo osname=linux\; >>$properties_file
+echo processor=x86-64, >>$properties_file
+
+# solaris x32 properties
+for i in `find extracted_natives/solaris-i586 -type f`
+do
+	echo solaris-i586/`basename $i`\; >>$properties_file
+done
+
+echo osname=solaris\; >>$properties_file
+echo processor=x86, >>$properties_file
+
+# solaris x64 properties
+for i in `find extracted_natives/solaris-amd64 -type f`
+do
+	echo solaris-amd64/`basename $i`\; >>$properties_file
+done
+
+echo osname=solaris\; >>$properties_file
+echo processor=x86-64, >>$properties_file
+
+# windows x64 properties
+for i in `find extracted_natives/windows-amd64 -type f`
+do
+	echo windows-amd64/`basename $i`\; >>$properties_file
+done
+
+echo osname=win32\; >>$properties_file
+echo processor=x86-64, >>$properties_file
+
+# windows x32 properties
+for i in `find extracted_natives/windows-i586 -type f`
+do
+	echo windows-i586/`basename $i`\; >>$properties_file
+done
+
+echo osname=win32\; >>$properties_file
+echo processor=x86, >>$properties_file
+
+# mac properties
+for i in `find extracted_natives/macosx-universal -type f`
+do
+	echo macosx-universal/`basename $i`\; >>$properties_file
+done
+
+echo osname=mac os x\; >>$properties_file
+echo processor=x86\; >>$properties_file
+echo processor=ppc >>$properties_file
+
+echo Fragment-Host=jogl >>$properties_file
 
 # repackage jar as OSGi bundle
-java -jar bnd.jar wrap -properties combined_jars/jogl-natives.jar.properties combined_jars/jogl-natives.jar
+java -jar bnd.jar wrap -properties $properties_file combined_jars/jogl-natives.jar
 
 # move it to bundles folder
 mv combined_jars/jogl-natives.bar bundles/jogl-natives.jar
