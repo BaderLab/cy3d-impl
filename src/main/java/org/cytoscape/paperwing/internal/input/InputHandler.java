84f1bf9f8110299c033af62667e449e8fed5db87
paperwing <paperwing@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-02-28 23:05:08 +0000

    Added javadoc explaining functions of InputHandler, InputProcessor

--- src/main/java/org/cytoscape/paperwing/internal/input/InputHandler.java
+++ src/main/java/org/cytoscape/paperwing/internal/input/InputHandler.java
@@ -2,6 +2,11 @@ package org.cytoscape.paperwing.internal.input;
 
 import org.cytoscape.paperwing.internal.data.GraphicsData;
 
+/**
+ * An InputHandler is capable of processing a given {@link GraphicsData} object using the given keyboard
+ * and mouse states. The processing done by an InputHandler is usually specific to a certain kind of input,
+ * such as input relating to camera movement or node selectioni.
+ */
 public interface InputHandler {
 	public void processInput(KeyboardMonitor keys, MouseMonitor mouse, GraphicsData graphicsData);
 }
