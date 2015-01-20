a4911c57b073b7b9036d5e64caa28238e96f1941
Mike Kucera <mikekucera@gmail.com>
2015-01-20 13:08:13 -0500

    Help hover icon.

--- src/main/java/org/baderlab/cy3d/internal/input/handler/ToolPanel.java
+++ src/main/java/org/baderlab/cy3d/internal/input/handler/ToolPanel.java
@@ -1,27 +1,39 @@
 package org.baderlab.cy3d.internal.input.handler;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.KeyStroke;
+import javax.swing.ToolTipManager;
+
+import org.baderlab.cy3d.internal.icons.IconManager;
+import org.baderlab.cy3d.internal.icons.IconManagerImpl;
 
 /**
- * Tool panel that overlays the renderer window.
+ * Toolbar panel that overlays the renderer window.
  * 
  * Responds to ALT and SHIFT as well.
  * 
@@ -40,6 +52,10 @@ public class ToolPanel {
 	private Set<MouseModeChangeListener> listeners = new LinkedHashSet<>();
 	
 	
+	/**
+	 * The input event listeners should use this to register
+	 * for events that indicate changes to the toolbar mode.
+	 */
 	public interface MouseModeChangeListener {
 		void mouseModeChanged(MouseMode mouseMode);
 	}
@@ -72,25 +88,49 @@ public class ToolPanel {
 		glass.setLayout(new BorderLayout());
 		glass.setVisible(true);
 		
-		ButtonGroup cameraGroup = new ButtonGroup();
+		JPanel toolPanel = new JPanel();
+		toolPanel.setLayout(new GridBagLayout());
+		toolPanel.setOpaque(false);
+		
+		// Camera mode buttons
 		JPanel cameraToolBar = new JPanel();
+		ButtonGroup cameraGroup = new ButtonGroup();
 		cameraToolBar.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		cameraToolBar.setOpaque(false);
 		
-		selectButton = createButton("Select", MouseMode.SELECT, cameraToolBar, cameraGroup);
-		cameraButton = createButton("Camera", MouseMode.CAMERA, cameraToolBar, cameraGroup);
+		selectButton = createModeButton("Select", MouseMode.SELECT, cameraGroup);
+		cameraButton = createModeButton("Camera", MouseMode.CAMERA, cameraGroup);
+		cameraToolBar.add(selectButton);
+		cameraToolBar.add(cameraButton);
 		
-		glass.add(cameraToolBar, BorderLayout.WEST);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 0.5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		toolPanel.add(cameraToolBar, gbc);
+		
+		// Help button
+		JLabel helpLabel = createHelpButton();
+		
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.weighty = 0.5;
+		gbc.anchor = GridBagConstraints.NORTHEAST;
+		toolPanel.add(helpLabel, gbc);
+		
+		glass.add(toolPanel, BorderLayout.NORTH);
 	}
 	
-	private JToggleButton createButton(String text, final MouseMode mode, Container parent, ButtonGroup group) {
+	
+	private JToggleButton createModeButton(String text, final MouseMode mode, ButtonGroup group) {
 		JToggleButton button = new JToggleButton(text);
 		button.setMargin(new Insets(0, 0, 0, 0));
 		button.setPreferredSize(new Dimension(40, 20));
 		button.setFont(new Font("Arial", Font.PLAIN, 8));
 		button.setFocusable(false);
 		group.add(button);
-		parent.add(button);
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -106,6 +146,65 @@ public class ToolPanel {
 		return button;
 	}
 
+	
+	private JLabel createHelpButton() {
+		String helpText;
+		try {
+			helpText = getHelpContents();
+		} catch(IOException e) {
+			e.printStackTrace();
+			return new JLabel();
+		}
+		
+		IconManager iconManager = new IconManagerImpl();
+		final JLabel questionLabel = new JLabel();
+		questionLabel.setText(IconManager.ICON_QUESTION_SIGN);
+		questionLabel.setFont(iconManager.getIconFont(14));
+		questionLabel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		questionLabel.setToolTipText(helpText);
+		
+		questionLabel.addMouseListener(new MouseAdapter() {
+			int restoreInitialDelay;
+			int restoreDismissDelay;
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				ToolTipManager ttManager = ToolTipManager.sharedInstance();
+				restoreInitialDelay = ttManager.getInitialDelay();
+				restoreDismissDelay = ttManager.getDismissDelay();
+				ttManager.setInitialDelay(0);
+				ttManager.setDismissDelay(60000);
+				MouseEvent me = new MouseEvent(questionLabel, MouseEvent.MOUSE_MOVED, System.currentTimeMillis(), 0, 0, 0, 0, false);
+				ttManager.mouseMoved(me);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				ToolTipManager ttManager = ToolTipManager.sharedInstance();
+				ttManager.setInitialDelay(restoreInitialDelay);
+				ttManager.setDismissDelay(restoreDismissDelay);
+			}
+		});
+		
+		return questionLabel;
+	}
+	
+	
+	private String getHelpContents() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/controls.html");
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buffer = new byte[512];
+		int length = 0;
+	    while ((length = in.read(buffer)) != -1) {
+	        out.write(buffer, 0, length);
+	    }
+	   
+	    out.close();
+	    in.close();
+		return new String(out.toByteArray(), "UTF-8");
+	}
+	
 	
 	@SuppressWarnings("serial")
 	private void setUpKeyboardInput(JInternalFrame frame) {
