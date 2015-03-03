c7d8973d837508892d0f2fb723d4b381a621ba15
Christian Lopes <chrtannus@gmail.com>
2015-03-03 16:12:49 -0500

    Cy3D Visual Lexicon now extends BasicVisualLexicon, because the VisualLexicon interface is not SPI, which means it's not safe for apps to implement it. Also implements getSupportedValueRange method.

--- src/main/java/org/baderlab/cy3d/internal/cytoscape/view/Cy3DVisualLexicon.java
+++ src/main/java/org/baderlab/cy3d/internal/cytoscape/view/Cy3DVisualLexicon.java
@@ -1,12 +1,23 @@
 package org.baderlab.cy3d.internal.cytoscape.view;
 
+import static java.util.Arrays.asList;
+import static org.cytoscape.view.presentation.property.LineTypeVisualProperty.DOT;
+import static org.cytoscape.view.presentation.property.LineTypeVisualProperty.EQUAL_DASH;
+import static org.cytoscape.view.presentation.property.LineTypeVisualProperty.SOLID;
+import static org.cytoscape.view.presentation.property.NodeShapeVisualProperty.ELLIPSE;
+import static org.cytoscape.view.presentation.property.NodeShapeVisualProperty.RECTANGLE;
+import static org.cytoscape.view.presentation.property.NodeShapeVisualProperty.TRIANGLE;
+
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.cytoscape.view.model.DiscreteRange;
 import org.cytoscape.view.model.NullDataType;
-import org.cytoscape.view.model.VisualLexicon;
-import org.cytoscape.view.model.VisualLexiconNode;
 import org.cytoscape.view.model.VisualProperty;
 import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 import org.cytoscape.view.presentation.property.NullVisualProperty;
@@ -18,18 +29,19 @@ import org.cytoscape.view.presentation.property.NullVisualProperty;
  * 
  * @author mkucera
  */
-public class Cy3DVisualLexicon implements VisualLexicon {
+public class Cy3DVisualLexicon extends BasicVisualLexicon {
 
 	/** The root visual property */
 	public static final VisualProperty<NullDataType> ROOT = new NullVisualProperty( "CY3D_ROOT", "cy3d Rendering Engine Root Visual Property");
 	
-	private final BasicVisualLexicon basicLexicon;
 	private final Set<VisualProperty<?>> supportedProps;
+	private final Map<VisualProperty<?>, Collection<?>> supportedValuesMap;
 	
 	
 	public Cy3DVisualLexicon() {
-		basicLexicon = new BasicVisualLexicon(ROOT);
+		super(ROOT);
 		supportedProps = new HashSet<>();
+		supportedValuesMap = new HashMap<>();
 		initSupportedProps();
 	}
 	
@@ -50,7 +62,7 @@ public class Cy3DVisualLexicon implements VisualLexicon {
 		supportedProps.add(BasicVisualLexicon.NODE_FILL_COLOR);
 		supportedProps.add(BasicVisualLexicon.NODE_LABEL);
 		supportedProps.add(BasicVisualLexicon.NODE_VISIBLE);
-		supportedProps.add(BasicVisualLexicon.NODE_SHAPE); // MKTODO this is the big one
+		supportedProps.add(BasicVisualLexicon.NODE_SHAPE);
 		
 		supportedProps.add(BasicVisualLexicon.NODE_SIZE);
 		supportedProps.add(BasicVisualLexicon.NODE_DEPTH);
@@ -58,39 +70,36 @@ public class Cy3DVisualLexicon implements VisualLexicon {
 		supportedProps.add(BasicVisualLexicon.NODE_HEIGHT);
 		
 		supportedProps.add(BasicVisualLexicon.EDGE_VISIBLE);
-		supportedProps.add(BasicVisualLexicon.EDGE_LINE_TYPE); // MKTODO this is also not all supported
+		supportedProps.add(BasicVisualLexicon.EDGE_LINE_TYPE);
 		supportedProps.add(BasicVisualLexicon.EDGE_SELECTED);
+		
+		supportedValuesMap.put(NODE_SHAPE, asList(RECTANGLE, ELLIPSE, TRIANGLE));
+		supportedValuesMap.put(EDGE_LINE_TYPE, asList(SOLID, DOT, EQUAL_DASH));
 	}
-	
 	
 	@Override
 	public boolean isSupported(VisualProperty<?> vp) {
-		return supportedProps.contains(vp) && basicLexicon.isSupported(vp);
+		return supportedProps.contains(vp) && super.isSupported(vp);
 	}
 	
 	@Override
-	public VisualProperty<NullDataType> getRootVisualProperty() {
-		return basicLexicon.getRootVisualProperty();
+	public <T> Set<T> getSupportedValueRange(VisualProperty<T> vp) {
+		if (vp.getRange() instanceof DiscreteRange) {
+			final DiscreteRange<T> range = (DiscreteRange<T>) vp.getRange();
+			final Collection<?> supportedList = supportedValuesMap.get(vp);
+			
+			if (supportedList != null) {
+				final Set<T> set = new LinkedHashSet<>();
+				
+				for (T value : range.values()) {
+					if (supportedList.contains(value))
+						set.add(value);
+				}
+				
+				return set;
+			}
+		}
+		
+		return Collections.emptySet();
 	}
-
-	@Override
-	public Set<VisualProperty<?>> getAllVisualProperties() {
-		return basicLexicon.getAllVisualProperties();
-	}
-
-	@Override
-	public VisualProperty<?> lookup(Class<?> type, String id) {
-		return basicLexicon.lookup(type, id);
-	}
-
-	@Override
-	public VisualLexiconNode getVisualLexiconNode(VisualProperty<?> vp) {
-		return basicLexicon.getVisualLexiconNode(vp);
-	}
-
-	@Override
-	public Collection<VisualProperty<?>> getAllDescendants(VisualProperty<?> prop) {
-		return basicLexicon.getAllDescendants(prop);
-	}
-
 }
